34c4ca5db4192805f0b649c1f673a51fe14ec076
Larry Goldstein <larry.goldstein@upslide.net>
2021-10-02 15:38:46 +0200

    Refacto by introducing an array of char delimiters in Split

--- StringCalculator3/Calculator.cs
+++ StringCalculator3/Calculator.cs
@@ -10,10 +10,7 @@ namespace StringCalculator3
             if (input.Length == 0)
                 return 0;
 
-            if (input.Contains("\n"))
-                return input.Split("\n").Select(num => int.Parse(num)).Sum();
-
-            return input.Split(',').Select(num => int.Parse(num)).Sum();
+            return input.Split(new char[] {',', '\n' }).Select(num => int.Parse(num)).Sum();
 
         }
     }
