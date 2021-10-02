4d1c148fd5dff7c5d1db941963b06c18645d9a9a
Larry Goldstein <larry.goldstein@upslide.net>
2021-10-02 16:36:43 +0200

    Refacto

--- StringCalculator3/Calculator.cs
+++ StringCalculator3/Calculator.cs
@@ -16,11 +16,12 @@ namespace StringCalculator3
             {
                 var customDelimiterStartIndex = 2;
                 var customDelimiterEndIndex = input.IndexOf("\n") - 1;
+                var startSecondLine = customDelimiterEndIndex + 2;
 
                 var customDelimiter =  input.Substring(customDelimiterStartIndex, customDelimiterEndIndex - customDelimiterStartIndex + 1);
                 delimiters = new string[] { ",", "\n", customDelimiter };
 
-                input = input.Substring(customDelimiterEndIndex + 1 + 1, (input.Length - 1) - (customDelimiterEndIndex + 2) + 1);
+                input = input.Substring(startSecondLine, (input.Length - 1) - startSecondLine + 1);
             }
 
             return input.Split(delimiters, StringSplitOptions.None).Select(num => int.Parse(num)).Sum();
