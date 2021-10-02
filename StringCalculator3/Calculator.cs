e8d32ab4b88faac2098d280fde67f732863bd278
Larry Goldstein <larry.goldstein@upslide.net>
2021-10-02 17:15:31 +0200

    Refacto by extracting ParseInput() method

--- StringCalculator3/Calculator.cs
+++ StringCalculator3/Calculator.cs
@@ -5,12 +5,30 @@ namespace StringCalculator3
 {
     public class Calculator
     {
+        private string[] _delimiters;
+        private string _secondLine;
+
+        public Calculator(string[] delimiters, string secondLine)
+        {
+            _delimiters = delimiters;
+            _secondLine = secondLine;
+        }
         public static int Sum(string input)
         {
             if (input.Length == 0)
                 return 0;
 
-            var delimiters = new string[] {",", "\n"};
+            var parsedData = ParseInput(input);
+
+            return parsedData._secondLine
+                             .Split(parsedData._delimiters, StringSplitOptions.None)
+                             .Select(num => int.Parse(num))
+                             .Sum();
+        }
+
+        private static Calculator ParseInput(string input)
+        {
+            var delimiters = new string[] { ",", "\n" };
 
             if (input.StartsWith("//"))
             {
@@ -18,13 +36,14 @@ namespace StringCalculator3
                 var customDelimiterEndIndex = input.IndexOf("\n") - 1;
                 var startSecondLine = customDelimiterEndIndex + 2;
 
-                var customDelimiter =  input.Substring(customDelimiterStartIndex, customDelimiterEndIndex - customDelimiterStartIndex + 1);
-                delimiters = new string[] { ",", "\n", customDelimiter };
+                var customDelimiter = input.Substring(customDelimiterStartIndex,
+                    customDelimiterEndIndex - customDelimiterStartIndex + 1);
 
+                delimiters = new string[] {",", "\n", customDelimiter};
                 input = input.Substring(startSecondLine);
             }
 
-            return input.Split(delimiters, StringSplitOptions.None).Select(num => int.Parse(num)).Sum();
+            return new Calculator(delimiters, input);
         }
     }
 }
